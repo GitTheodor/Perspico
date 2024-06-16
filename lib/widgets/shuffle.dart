d20eb024499e2ca74e15e19e6a33da19114ab5c5
Theodor <beleacovtheodor77@gmail.com>
2024-06-16 16:02:24 +0300

    Proiect

---
+++ lib/widgets/shuffle.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Shuffle extends StatelessWidget {
+  final VoidCallback shuffle;
+
+  const Shuffle(this.shuffle, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton.icon(
+        onPressed: shuffle,
+        icon: const Icon(
+          CupertinoIcons.restart,
+          size: 20.0,
+        ),
+        label: const Text(
+          "RESET ",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.white, shape: const StadiumBorder(),
+          backgroundColor: Colors.deepPurpleAccent,
+        ),
+      ),
+    );
+  }
+}
